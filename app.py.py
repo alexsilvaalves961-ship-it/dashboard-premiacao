73203b8ee612892e967faaef243b5bfa0889b366
alexsilvaalves961-ship-it <alexsilvaalves961@gmail.com>
2026-08-25 08:18:10 -0300

    Update app.py.py

--- app.py.py
+++ app.py.py
@@ -5,7 +5,7 @@ from dataclasses import dataclass
 from datetime import datetime
 from typing import Optional, Tuple
 
-import gradio as gr
+import streamlit as st
 import numpy as np
 import openpyxl
 import pandas as pd
